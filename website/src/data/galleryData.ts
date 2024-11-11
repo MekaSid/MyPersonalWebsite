cfd8906842a0e2f00ce6da0bcdcd8390b691652d
MekaSid <sidmekarao@gmail.com>
2024-11-11 10:49:25 +0100

    new photos

--- website/src/data/galleryData.ts
+++ website/src/data/galleryData.ts
@@ -19,6 +19,42 @@ export interface Photo {
     { src: '/gallery/rathaus.jpg', alt: 'Munich' , caption: "Rathaus-Glockenspiel, Munich"},
     { src: '/gallery/marienplatz.jpg', alt: 'Munich' , caption: "Marienplatz, Munich"},
     { src: '/gallery/giovanni.jpg', alt: 'Florence' , caption: "San Giovanni, Florence"},
+    { src: '/gallery/redtruck.jpg', alt: 'Billund' , caption: "Gota Hotel, Billund"},
+    { src: '/gallery/ninjago.jpg', alt: 'Billund' , caption: "Ninjago, Billund"},
+    { src: '/gallery/ferrari.jpg', alt: 'Billund' , caption: "Ferrari, Billund"},
+    { src: '/gallery/flyingeagle.jpg', alt: 'Billund' , caption: "Flying Eagle, Billund"},
+    { src: '/gallery/stpetersqr.jpg', alt: 'Vatican' , caption: "St. Peter's Square, Vatican"},
+    { src: '/gallery/santangelo.jpg', alt: 'Rome' , caption: "Sant'Angelo, Rome"},
+    { src: '/gallery/capitolehill.jpg', alt: 'Rome' , caption: "Capitoline Hill, Rome"},
+    { src: '/gallery/santamaria.jpg', alt: 'Florence' , caption: "Santa Maria, Florence"},
+    { src: '/gallery/ducks.jpg', alt: 'Barcelona' , caption: "The Cathedral, Barcelona"},
+    { src: '/gallery/ciutat.jpg', alt: 'Barcelona' , caption: "Ciutat Vella, Barcelona"},
+    { src: '/gallery/guell.jpg', alt: 'Barcelona' , caption: "Park Güell, Barcelona"},
+    { src: '/gallery/flowers.jpg', alt: 'Barcelona' , caption: "Art Museum, Barcelona"},
+    { src: '/gallery/cathedralmilan.jpg', alt: 'Milan' , caption: "The Catherdral, Milan"},
+    { src: '/gallery/starbucks.jpg', alt: 'Milan' , caption: "Reserve Roastery, Milan"},
+    { src: '/gallery/lion.jpg', alt: 'Luzern' , caption: "The Lion Monument, Luzern"},
+    { src: '/gallery/egypt.jpg', alt: 'Billund' , caption: "The Pharaoh, Billund"},
+    { src: '/gallery/pisa.jpg', alt: 'Pisa' , caption: "Torre, Pisa"},
+    { src: '/gallery/uffizi.jpg', alt: 'Florence' , caption: "Uffizi Gallery, Florence"},
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
 
 
 
